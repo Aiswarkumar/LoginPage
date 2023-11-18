4dfc8f37938ed747c618cd5f8b4c604f765e0d9c
Aiswarkumar <aiswardurga30@gmail.com>
2023-11-19 01:03:35 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Another } from './Another';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Another/>
+    </div>
+  );
+}
+
+export default App;
